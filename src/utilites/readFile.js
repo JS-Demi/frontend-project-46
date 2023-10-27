8419dbddb787809f5f4655defd2e27c46d2afec4
Dmitry <dmt.ivanov1@yandex.ru>
2023-10-27 23:03:06 +0300

    update reading with new parser

--- src/utilites/readFile.js
+++ src/utilites/readFile.js
@@ -1,9 +1,18 @@
 import fs from 'fs';
-import parseFile from './parsers.js';
+import path from 'path';
+import { jsonParse, yamlParse } from './parsers.js';
 
-const readFile = (pathfile) => {
-  const fileContent = fs.readFileSync(pathfile, { encoding: 'utf8' });
-  const parsed = parseFile(fileContent);
+const readFile = (filepath) => {
+  const fileContent = fs.readFileSync(filepath, { encoding: 'utf8' });
+  const format = path.extname(filepath);
+  let parsed;
+  if (format === '.json') {
+    parsed = jsonParse(fileContent);
+  } else if (format === '.yml') {
+    parsed = yamlParse(fileContent);
+  } else if (format === '.yaml') {
+    parsed = yamlParse(fileContent);
+  }
   return parsed;
 };
 
