7b7d0c32797df7011275f8c43667ef5170c5253a
Dmitry <dmt.ivanov1@yandex.ru>
2023-11-02 12:31:04 +0300

    updated tests for nested

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -10,15 +10,48 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const diff = {
-  "- age": 27,
-  "+ age": "unknown",
-  city: "Moscow",
-  '+ developer': true,
-  "- name": "Dmitry",
-  "+ name": "Irene",
-  "- qualification": "rescuer",
-  "- work": "MES",
-  "+ work": "Hexlet",
+  common: {
+    "+ follow": false,
+    setting1: "Value 1",
+    "- setting2": 200,
+    "- setting3": true,
+    '+ setting3': null,
+    '+ setting4': "blah blah",
+    '+ setting5': {
+      key5: 'value5',
+    },
+    setting6: {
+      doge: {
+        "- wow": "",
+        "+ wow": "so much",
+      },
+      key: "value",
+      "+ ops": "vops",
+    },
+  },
+  group1: {
+    "- baz": "bas",
+    "+ baz": "bars",
+    foo: "bar",
+    "- nest": {
+      key: "value",
+    },
+    "+ nest": "str",
+  },
+  "- group2": {
+    abc: 12345,
+    deep: {
+      id: 45,
+    },
+  },
+  "+ group3": {
+    deep: {
+      id: {
+        number: 45,
+      },
+    },
+    fee: 100500,
+  },
 };
 
 const result = JSON.stringify(diff, null, ' ').replaceAll('"', '').replaceAll(',', '');
@@ -26,8 +59,8 @@ const result = JSON.stringify(diff, null, ' ').replaceAll('"', '').replaceAll(',
 test('genDiff.js', () => {
   const filepath1 = getPathToFixtures('testFile1.json');
   const filepath2 = getPathToFixtures('testFile2.json');
-  const differ = genDiff(filepath1, filepath2);
-  expect(differ).toBe(result);
+  const difference = genDiff(filepath1, filepath2);
+  expect(difference).toBe(result);
 });
 
 test('getPathTofile.js', () => {
@@ -39,7 +72,17 @@ test('getPathTofile.js', () => {
 
 test('genDiff.js yaml files', () => {
   const filepath1 = getPathToFixtures('testFile3.yaml');
-  const filepath2 = getPathToFixtures('testFile4.yml');
-  const difference = genDiff(filepath1, filepath2);
-  expect(difference).toBe(result);
+  const filepath2 = getPathToFixtures('testFile2.json');
+  const filepath3 = getPathToFixtures('testFile4.yml');
+  const checkDifference1 = genDiff(filepath1, filepath2);
+  const checkDifference2 = genDiff(filepath3, filepath2);
+  expect(checkDifference1).toBe(result);
+  expect(checkDifference2).toBe(result);
 });
+
+// test('unknwon format genDiff.js', () => {
+//   const filepath1 = getPathToFixtures('testFile1.json');
+//   const filepath2 = getPathToFixtures('testFile2.json');
+//   const difference = genDiff(filepath1, filepath2, 'json');
+//   expect(difference).toThrow('Неправильно выбран формат');
+// });
