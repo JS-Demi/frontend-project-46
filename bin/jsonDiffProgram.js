19e90d6c1757868e9f62fdcbb7ae8f05d2d7974a
Dmitry <dmt.ivanov1@yandex.ru>
2023-11-02 12:31:22 +0300

    add stylish as defalut

--- bin/jsonDiffProgram.js
+++ bin/jsonDiffProgram.js
@@ -10,7 +10,7 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.1')
-  .option('-f, --format [type]', 'output format (default: "stylish")')
+  .option('-f, --format [type]', 'output format (default: "stylish")', 'stylish')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((file1, file2) => {
