a4f37f9f139a5e07aa33364371b60226bd99054f
Dmitry <dmt.ivanov1@yandex.ru>
2023-11-04 19:59:33 +0300

    fix space issues in tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -57,6 +57,51 @@ const json = {
   },
 };
 
+const stylishFormat = [`{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`,
+];
 test('getPathTofile.js', () => {
   const pathToFixtures = path.join(__dirname, '..', '__fixtures__', 'testFile1.json');
   const pathToSrc = path.join(__dirname, '..', 'src', 'file1.json');
@@ -65,7 +110,7 @@ test('getPathTofile.js', () => {
 });
 
 test('is correct output stylish format', () => {
-  const expectedStylishFormat = JSON.stringify(json, null, ' ').replaceAll('"', '').replaceAll(',', '');
+  const expectedStylishFormat = stylishFormat.join('');
   expect(genDiff(pathToJsonFile1, pathToJsonFile2)).toBe(expectedStylishFormat);
   expect(genDiff(pathToYmlFile1, pathToJsonFile2)).toBe(expectedStylishFormat);
 });
