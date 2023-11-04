2e70eb45589b1c95f6e66043b8e3670804de43ad
Dmitry <dmt.ivanov1@yandex.ru>
2023-11-04 13:42:06 +0300

    small updates

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,4 +1,3 @@
-/* eslint-disable quotes */
 /* eslint-disable no-undef */
 /* eslint-disable no-underscore-dangle */
 import path from 'path';
@@ -9,42 +8,46 @@ import { getPathToFixtures, getPathToSrc } from '../src/utilites/getPathToFile.j
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const diff = {
+const pathToJsonFile1 = getPathToFixtures('testFile1.json');
+const pathToJsonFile2 = getPathToFixtures('testFile2.json');
+const pathToYmlFile1 = getPathToFixtures('testFile3.yaml');
+const pathToYmlFile2 = getPathToFixtures('testFile4.yml');
+const json = {
   common: {
-    "+ follow": false,
-    setting1: "Value 1",
-    "- setting2": 200,
-    "- setting3": true,
+    '+ follow': false,
+    setting1: 'Value 1',
+    '- setting2': 200,
+    '- setting3': true,
     '+ setting3': null,
-    '+ setting4': "blah blah",
+    '+ setting4': 'blah blah',
     '+ setting5': {
       key5: 'value5',
     },
     setting6: {
       doge: {
-        "- wow": "",
-        "+ wow": "so much",
+        '- wow': '',
+        '+ wow': 'so much',
       },
-      key: "value",
-      "+ ops": "vops",
+      key: 'value',
+      '+ ops': 'vops',
     },
   },
   group1: {
-    "- baz": "bas",
-    "+ baz": "bars",
-    foo: "bar",
-    "- nest": {
-      key: "value",
+    '- baz': 'bas',
+    '+ baz': 'bars',
+    foo: 'bar',
+    '- nest': {
+      key: 'value',
     },
-    "+ nest": "str",
+    '+ nest': 'str',
   },
-  "- group2": {
+  '- group2': {
     abc: 12345,
     deep: {
       id: 45,
     },
   },
-  "+ group3": {
+  '+ group3': {
     deep: {
       id: {
         number: 45,
@@ -54,15 +57,6 @@ const diff = {
   },
 };
 
-const expectedStylishFormat = JSON.stringify(diff, null, ' ').replaceAll('"', '').replaceAll(',', '');
-
-test('genDiff.js', () => {
-  const filepath1 = getPathToFixtures('testFile1.json');
-  const filepath2 = getPathToFixtures('testFile2.json');
-  const difference = genDiff(filepath1, filepath2);
-  expect(difference).toBe(expectedStylishFormat);
-});
-
 test('getPathTofile.js', () => {
   const pathToFixtures = path.join(__dirname, '..', '__fixtures__', 'testFile1.json');
   const pathToSrc = path.join(__dirname, '..', 'src', 'file1.json');
@@ -70,39 +64,36 @@ test('getPathTofile.js', () => {
   expect(getPathToSrc('file1.json')).toBe(pathToSrc);
 });
 
-test('genDiff.js yaml files', () => {
-  const filepath1 = getPathToFixtures('testFile3.yaml');
-  const filepath2 = getPathToFixtures('testFile2.json');
-  const filepath3 = getPathToFixtures('testFile4.yml');
-  const checkDifference1 = genDiff(filepath1, filepath2);
-  const checkDifference2 = genDiff(filepath3, filepath2);
-  expect(checkDifference1).toBe(expectedStylishFormat);
-  expect(checkDifference2).toBe(expectedStylishFormat);
+test('is correct output stylish format', () => {
+  const expectedStylishFormat = JSON.stringify(json, null, ' ').replaceAll('"', '').replaceAll(',', '');
+  expect(genDiff(pathToJsonFile1, pathToJsonFile2)).toBe(expectedStylishFormat);
+  expect(genDiff(pathToYmlFile1, pathToJsonFile2)).toBe(expectedStylishFormat);
 });
 
-// test('unknwon format genDiff.js', () => {
-//   const filepath1 = getPathToFixtures('testFile1.json');
-//   const filepath2 = getPathToFixtures('testFile2.json');
-//   const difference = genDiff(filepath1, filepath2, 'json');
-//   expect(difference).toThrow('Неправильно выбран формат');
-// });
+test('unknown format type', () => {
+  const nonExistantFormat = genDiff(pathToJsonFile1, pathToJsonFile2, 'blabla');
+  expect(nonExistantFormat).toThrow(Error);
+});
 
 const expectedPlainFormat = [
-  `Property 'common.follow' was added with value: false`,
-  `Property 'common.setting2' was removed`,
-  `Property 'common.setting3' was updated. From true to null`,
-  `Property 'common.setting4' was added with value: 'blah blah'`,
-  `Property 'common.setting5' was added with value: [complex value]`,
-  `Property 'common.setting6.doge.wow' was updated. From '' to 'so much'`,
-  `Property 'common.setting6.ops' was added with value: 'vops'`,
-  `Property 'group1.baz' was updated. From 'bas' to 'bars'`,
-  `Property 'group1.nest' was updated. From [complex value] to 'str'`,
-  `Property 'group2' was removed`,
-  `Property 'group3' was added with value: [complex value]`,
+  'Property \'common.follow\' was added with value: false',
+  'Property \'common.setting2\' was removed',
+  'Property \'common.setting3\' was updated. From true to null',
+  'Property \'common.setting4\' was added with value: \'blah blah\'',
+  'Property \'common.setting5\' was added with value: [complex value]',
+  'Property \'common.setting6.doge.wow\' was updated. From \'\' to \'so much\'',
+  'Property \'common.setting6.ops\' was added with value: \'vops\'',
+  'Property \'group1.baz\' was updated. From \'bas\' to \'bars\'',
+  'Property \'group1.nest\' was updated. From [complex value] to \'str\'',
+  'Property \'group2\' was removed',
+  'Property \'group3\' was added with value: [complex value]',
 ];
 
-test('genDiff.js plain format', () => {
-  const filepath1 = getPathToFixtures('testFile1.json');
-  const filepath2 = getPathToFixtures('testFile2.json');
-  expect(genDiff(filepath1, filepath2, 'plain')).toBe(expectedPlainFormat.join('\n'));
+test('is correct output plain format', () => {
+  expect(genDiff(pathToJsonFile1, pathToJsonFile2, 'plain')).toBe(expectedPlainFormat.join('\n'));
+  expect(genDiff(pathToYmlFile2, pathToJsonFile2, 'plain')).toBe(expectedPlainFormat.join('\n'));
+});
+
+test('is correct ouput JSON format', () => {
+  expect(genDiff(pathToJsonFile1, pathToJsonFile2, 'json')).toStrictEqual(json);
 });
--- src/genDiff.js
+++ src/genDiff.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 import _ from 'lodash';
 import readFile from './utilites/readFile.js';
-import { stylishFormat, plainFormat } from './formatters/index.js';
+import { stylishFormat, plainFormat, jsonFormat } from './formatters/index.js';
 
 const genDiff = (filepath1, filepath2, formatter = 'stylish') => {
   const contentFile1 = readFile(filepath1);
@@ -11,27 +11,28 @@ const genDiff = (filepath1, filepath2, formatter = 'stylish') => {
     const keys1 = Object.keys(file1);
     const keys2 = Object.keys(file2);
     const keys = _.uniq([...keys1, ...keys2]);
-    keys.map((key) => {
-      const remove = `- ${key}`;
-      const add = `+ ${key}`;
-      if (!keys2.includes(key)) {
-        result[remove] = file1[key];
-      }
-      if (!keys1.includes(key)) {
-        result[add] = file2[key];
-      }
-      if (keys2.includes(key) && keys1.includes(key)) {
-        if (!_.isObject(file2[key])) {
-          if (file1[key] !== file2[key]) {
-            result[`-${remove}`] = file1[key];
-            result[`+${add}`] = file2[key];
-          } else {
-            result[key] = file2[key];
-          }
-        } else result[key] = iter(file1[key], file2[key]);
-      }
-      return result;
-    });
+    _.sortBy(keys, (value) => value)
+      .map((key) => {
+        const remove = `- ${key}`;
+        const add = `+ ${key}`;
+        if (!keys2.includes(key)) {
+          result[remove] = file1[key];
+        }
+        if (!keys1.includes(key)) {
+          result[add] = file2[key];
+        }
+        if (keys2.includes(key) && keys1.includes(key)) {
+          if (!_.isObject(file2[key])) {
+            if (file1[key] !== file2[key]) {
+              result[`-${remove}`] = file1[key];
+              result[`+${add}`] = file2[key];
+            } else {
+              result[key] = file2[key];
+            }
+          } else result[key] = iter(file1[key], file2[key]);
+        }
+        return result;
+      });
     return { ...result };
   };
   switch (formatter) {
@@ -39,8 +40,10 @@ const genDiff = (filepath1, filepath2, formatter = 'stylish') => {
       return stylishFormat(iter(contentFile1, contentFile2));
     case 'plain':
       return plainFormat(iter(contentFile1, contentFile2));
+    case 'json':
+      return jsonFormat(iter(contentFile1, contentFile2));
     default:
-      throw new Error('Неправильно выбран формат');
+      throw new Error('Unknown format type');
   }
 };
 export default genDiff;
