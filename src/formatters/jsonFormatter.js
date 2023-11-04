ca458fc57f2b81e4390cc382b4dc6efcfe2e0867
Dmitry <dmt.ivanov1@yandex.ru>
2023-11-04 19:58:57 +0300

    update json formatter

--- src/formatters/jsonFormatter.js
+++ src/formatters/jsonFormatter.js
@@ -1,8 +1,8 @@
 import { jsonParse } from '../utilites/parsers.js';
 
 const formatJSON = (data) => {
-  const str = JSON.stringify(data);
-  const cleaned = str.replaceAll('--', '-').replaceAll('++', '+');
-  return jsonParse(cleaned);
+  const str = JSON.stringify(data).replaceAll('--', '-').replaceAll('++', '+');
+  const parsed = jsonParse(str);
+  return parsed;
 };
 export default formatJSON;
