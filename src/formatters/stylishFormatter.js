13f74f1bbdfd07e53791f6aa298819124a17be10
Dmitry <dmt.ivanov1@yandex.ru>
2023-11-04 18:42:58 +0300

    fix spaces

--- src/formatters/stylishFormatter.js
+++ src/formatters/stylishFormatter.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const formatterStylish = (data, replacer = ' ', spaceCount = 1) => {
+const formatterStylish = (data, replacer = ' ', spaceCount = 4) => {
   const iter = (currentText, depth) => {
     if (!_.isObject(currentText)) {
       return `${currentText}`;
@@ -9,7 +9,9 @@ const formatterStylish = (data, replacer = ' ', spaceCount = 1) => {
     const currentIndent = replacer.repeat(indentSize);
     const bracketsIndent = replacer.repeat(indentSize - spaceCount);
     const lines = Object.entries(currentText)
-      .map(([key, value]) => `${currentIndent}${key}: ${iter(value, depth + 1)}`);
+      .map(([key, value]) => (key.startsWith('+') || key.startsWith('-')
+        ? `${replacer.repeat(depth * spaceCount - 2)}${key}: ${iter(value, depth + 1)}`
+        : `${currentIndent}${key}: ${iter(value, depth + 1)}`));
     return [
       '{',
       ...lines,
