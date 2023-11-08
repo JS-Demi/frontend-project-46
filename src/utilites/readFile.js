bf6506ad95420620bc58d7eddcfbf401ec9f942e
Dmitry <dmt.ivanov1@yandex.ru>
2023-11-08 14:07:31 +0300

    now its only read file

--- src/utilites/readFile.js
+++ src/utilites/readFile.js
@@ -1,18 +1,9 @@
 import fs from 'fs';
-import path from 'path';
-import { jsonParse, yamlParse } from './parsers.js';
+import getFixturesPath from './getFixturesPath.js';
 
-const readFile = (filepath) => {
+const readFile = (file) => {
+  const filepath = getFixturesPath(file);
   const fileContent = fs.readFileSync(filepath, { encoding: 'utf8' });
-  const format = path.extname(filepath);
-  let parsed;
-  if (format === '.json') {
-    parsed = jsonParse(fileContent);
-  } else if (format === '.yml') {
-    parsed = yamlParse(fileContent);
-  } else if (format === '.yaml') {
-    parsed = yamlParse(fileContent);
-  }
-  return parsed;
+  return fileContent;
 };
 export default readFile;
