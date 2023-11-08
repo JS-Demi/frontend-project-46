79825c95dbd363cb103a80baa49a7593a5723282
Dmitry <dmt.ivanov1@yandex.ru>
2023-11-08 14:11:00 +0300

    fix main mistake with code

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,49 +1,43 @@
 /* eslint-disable no-underscore-dangle */
 import _ from 'lodash';
+import parse from './utilites/parsers.js';
 import readFile from './utilites/readFile.js';
-import { stylishFormat, plainFormat, jsonFormat } from './formatters/index.js';
+import selectedFormat from './formatters/index.js';
 
-const genDiff = (filepath1, filepath2, formatter = 'stylish') => {
-  const contentFile1 = readFile(filepath1);
-  const contentFile2 = readFile(filepath2);
-  const iter = (file1, file2) => {
-    const result = {};
-    const keys1 = Object.keys(file1);
-    const keys2 = Object.keys(file2);
-    const keys = _.uniq([...keys1, ...keys2]);
-    _.sortBy(keys, (value) => value)
-      .map((key) => {
-        const remove = `- ${key}`;
-        const add = `+ ${key}`;
-        if (!keys2.includes(key)) {
-          result[remove] = file1[key];
-        }
-        if (!keys1.includes(key)) {
-          result[add] = file2[key];
-        }
-        if (keys2.includes(key) && keys1.includes(key)) {
-          if (!_.isObject(file2[key])) {
-            if (file1[key] !== file2[key]) {
-              result[`-${remove}`] = file1[key];
-              result[`+${add}`] = file2[key];
-            } else {
-              result[key] = file2[key];
-            }
-          } else result[key] = iter(file1[key], file2[key]);
-        }
-        return result;
-      });
-    return { ...result };
-  };
-  switch (formatter) {
-    case 'stylish':
-      return stylishFormat(iter(contentFile1, contentFile2));
-    case 'plain':
-      return plainFormat(iter(contentFile1, contentFile2));
-    case 'json':
-      return jsonFormat(iter(contentFile1, contentFile2));
-    default:
-      throw new Error('Unknown format type');
-  }
-};
+function diff(oldObj, newObj) {
+  const keys = _.sortBy(Object.keys({ ...oldObj, ...newObj }), (key) => key);
+  const tree = keys.map((key) => {
+    const oldValue = oldObj[key];
+    const newValue = newObj[key];
+    if (_.has(newObj, key) && !_.has(oldObj, key)) {
+      return { key, type: 'created', value: newValue };
+    }
+    if (!_.has(newObj, key) && _.has(oldObj, key)) {
+      return { key, type: 'removed', value: oldValue };
+    }
+    if (_.has(newObj, key) && _.has(oldObj, key)) {
+      if (oldValue === newValue) {
+        return { key, type: 'unchanged', value: oldValue };
+      }
+      if (_.isObject(oldValue) && _.isObject(newValue)) {
+        return { key, type: 'nested', children: diff(oldValue, newValue) };
+      }
+    }
+    const updated = {
+      key, type: 'updated', oldValue, newValue,
+    };
+    return updated;
+  });
+  return tree;
+}
+
+function genDiff(file1, file2, format = 'stylish') {
+  const contentFile1 = readFile(file1);
+  const contentFile2 = readFile(file2);
+  const parsedFile1 = parse(file1, contentFile1);
+  const parsedFile2 = parse(file2, contentFile2);
+  const differenceTree = diff(parsedFile1, parsedFile2);
+  const result = selectedFormat(differenceTree, format);
+  return result;
+}
 export default genDiff;
