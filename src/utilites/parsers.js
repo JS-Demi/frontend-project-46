cfe9921a13797fdbfa28acc4c6b4fd5ac13ab713
Dmitry <dmt.ivanov1@yandex.ru>
2023-11-08 14:08:21 +0300

    now it can parse different format

--- src/utilites/parsers.js
+++ src/utilites/parsers.js
@@ -1,5 +1,21 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import yaml from 'js-yaml';
+import path from 'path';
 
 export const jsonParse = (file) => JSON.parse(file);
-export const yamlParse = (file) => yaml.load(file);
+const yamlParse = (file) => yaml.load(file);
+
+const parse = (fileName, file) => {
+  const format = path.extname(fileName);
+  if (format === '.json') {
+    return jsonParse(file);
+  }
+  if (format === '.yml') {
+    return yamlParse(file);
+  }
+  if (format === '.yaml') {
+    return yamlParse(file);
+  }
+  return file;
+};
+
+export default parse;
