d2c92ff7b59134bec84f7e77437467747686998c
Dmitry <dmt.ivanov1@yandex.ru>
2023-11-08 15:46:49 +0300

    fix

--- src/getFixturesPath.js
+++ src/getFixturesPath.js
@@ -4,6 +4,6 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const getFixturesPath = (file) => path.join(__dirname, '..', '__fixtures__', file);
+const getFixturesPath = (file) => path.resolve(__dirname, '..', '__fixtures__', file);
 
 export default getFixturesPath;
